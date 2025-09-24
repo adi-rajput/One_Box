247aeb7913f1ed6cd73bb15ea4a22e431795d8cb
Aditya Rajput <ar9412349716@gmail.com>
2025-09-24 19:23:35 +0530

    FEAT: APIs for 1. Searching 2. Getting all paginated 3. Filtered paginated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,10 +41,70 @@ app.get('/search', async (req: Request, res: Response) => {
     }
 });
 
+app.get('/get-all-email', async (req: Request, res: Response) => {
+    try {
+        const page = parseInt(req.query.page as string, 10) || 1
+
+        const pageSize = 100;
+        const from = (page - 1) * pageSize;
+
+        const esQuery = {
+            from: from,
+            size: pageSize,
+            query: {
+                match_all: {}
+            },
+            sort: [
+                { date: { order: "desc" } }
+            ]
+        }
+
+        const output = await searchEmails(esQuery);
+
+        res.status(200).json(output.hits.hits.map((hit: any) => hit._source));
+    } catch (error) {
+        console.error('Error in root endpoint:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+app.get("/get-filtered-emails", async (req: Request, res: Response) => {
+    try {
+        const { category, accountId, folder, page = 1 } = req.query;
+        const filters: any[] = [];
+        if (category) {
+            filters.push({ term: { category } });
+        }
+        if (accountId) {
+            filters.push({ term: { accountId } });
+        }
+        if (folder) {
+            filters.push({ term: { folder } });
+        }
+        const from = (Number(page) - 1) * 100;
+        const esQuery: any = {
+            from: from,
+            size: 100,
+            bool: {
+                filters: filters
+            },
+            sort: [
+                { date: { order: "desc" } }
+            ]
+        }
+
+        const output = await searchEmails(esQuery);
+        res.status(200).json(output.hits.hits.map((hit: any) => hit._source));
+    } catch (error) {
+        console.error('Error in /get-filtered-emails endpoint:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 async function startServer() {
     try {
         await indexer();
-        await startImapSync();
+        // await startImapSync();
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
